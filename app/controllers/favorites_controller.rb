b7961d444364041d395ef5b3b9b414367037d2f1
koki.kotake <koreanfrom93@gmail.com>
2021-03-17 05:30:28 +0000

    debug

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     recipe = Recipe.find(params[:recipe_id])
     current_user.fav(recipe)
     flash[:success] = 'お気に入りに追加しました。'
-    redirect_to recipe
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     recipe = Recipe.find(params[:recipe_id])
     current_user.unfav(recipe)
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_to recipe
+    redirect_back(fallback_location: root_path)
   end
 end
