99080269bab65a966f09c7df7f77a244d0c37c93
koki.kotake <koreanfrom93@gmail.com>
2021-03-19 05:41:27 +0000

    cloudinary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,4 +65,9 @@ private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
   end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
 end
