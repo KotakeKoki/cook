70995f29844e88f1553f27057249e98d00e90024
koki.kotake <koreanfrom93@gmail.com>
2021-03-19 11:43:19 +0000

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :edit, :followings, :followers]
-  before_action :correct_user, only: [:destroy,  :update]
+  before_action :correct_user, only: [:destroy,  :update, :edit]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
